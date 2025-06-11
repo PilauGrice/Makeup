a677021b20bc62bda4074d64b897f4855a329b15
PilauGrice <cal@lincswebdev.co.uk>
2025-06-11 20:44:29 +0100

    adding some color via css and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './resources/views/**/*.antlers.html',
+    './content/**/*.md',
+  ],
+  theme: {
+    extend: {
+        colors: {
+        // Add your custom colors here
+        brandPink: '#dc9eab',
+    },
+  },
+},
+  plugins: [require('@tailwindcss/typography')],
+};
