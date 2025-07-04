17b533d49b4bcfe10bfe74c081b3c9926df210f5
PilauGrice <cal@lincswebdev.co.uk>
2025-07-04 16:53:57 +0100

    trying to fix @theme on server

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './resources/views/**/*.antlers.html',
@@ -5,12 +6,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
-        colors: {
-        // Add your custom colors here
+      colors: {
         brandPink: '#dc9eab',
+        twoPink: '#fce6e6',
+        darkPink: '#A3004C',
+      },
+      backgroundImage: {
+        heroRight: "url('/assets/darkfeather.jpg')",
+      },
+      screens: {
+        'md-lg': '991px',
+        '1lg': '1200px',
+      },
     },
   },
-},
   plugins: [require('@tailwindcss/typography')],
-};
+}
