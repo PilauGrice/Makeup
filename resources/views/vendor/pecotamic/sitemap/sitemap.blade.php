f295f073ecb8f6d7d9f6e1746bf84c343dea1335
Cally PilauGrice <Cal@lincswebdev.co.uk>
2025-07-17 19:56:10 +0100

    fixing

---
+++ resources/views/vendor/pecotamic/sitemap/sitemap.blade.php
@@ -0,0 +1,26 @@
+{!! $xml_header !!}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 
+                            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+@foreach ($entries as $entry)
+    <url>
+        <loc>{{ $entry->loc }}</loc>
+        <lastmod>{{ $entry->lastmod->format('c') }}</lastmod>
+        @if ($entry->changefreq)
+            <changefreq>{{ $entry->changefreq }}</changefreq>
+        @endif
+        <priority>
+            @if ($entry->priority)
+                {{ number_format($entry->priority, 1) }}
+            @elseif ($entry->path === '/')
+                1.0
+            @else
+                0.8
+            @endif
+        </priority>
+    </url>
+@endforeach
+
+</urlset>
